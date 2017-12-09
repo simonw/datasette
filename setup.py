446f4b832272b2286f6f65af19714eb64afb7aa6
Simon Willison <simonw@eventbrite.com>
2017-12-08 19:00:33 -0800

    Upgrade to Sanic 0.7.0 (#168)
    https://github.com/channelcat/sanic/releases/tag/0.7.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     install_requires=[
         'click==6.7',
         'click-default-group==1.2',
-        'Sanic==0.6.0',
+        'Sanic==0.7.0',
         'Jinja2==2.10',
         'hupper==1.0',
     ],

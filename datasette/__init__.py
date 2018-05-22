d0d19453e8623cc98a2baa2cadaaff4cd2fee973
Robert Gieseke <rob.g@web.de>
2018-05-22 19:33:57 +0200

    Move version info back to separate module

--- datasette/__init__.py
+++ datasette/__init__.py
@@ -1,7 +1,3 @@
+from datasette.version import __version_info__, __version__  # noqa
 from .hookspecs import hookimpl # noqa
 from .hookspecs import hookspec # noqa
-
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
-__version_info__ = tuple(__version__.split("."))
---
+++ datasette/version.py
@@ -0,0 +1,6 @@
+from ._version import get_versions
+
+__version__ = get_versions()['version']
+del get_versions
+
+__version_info__ = tuple(__version__.split("."))

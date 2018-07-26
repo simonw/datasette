2302b76a2e14ca8b857268e1ffcafb9c2080fc80
Simon Willison <swillison@gmail.com>
2018-07-26 06:10:34 -0700

    New dependencies: starlette and uvicorn

--- setup.py
+++ setup.py
@@ -41,6 +41,8 @@ setup(
         'hupper==1.0',
         'pint==0.8.1',
         'pluggy>=0.1.0,<1.0',
+        'starlette==0.1.14',
+        'uvicorn==0.2.21',
     ],
     entry_points='''
         [console_scripts]

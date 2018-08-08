848ed0e0420d2e8c95a96b4cf73082da4c65d8f6
Simon Willison <swillison@gmail.com>
2018-08-07 17:51:17 -0700

    Bump pluggy

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
         'Jinja2==2.10',
         'hupper==1.0',
         'pint==0.8.1',
-        'pluggy>=0.1.0,<1.0',
+        'pluggy>=0.7.1',
     ],
     entry_points='''
         [console_scripts]

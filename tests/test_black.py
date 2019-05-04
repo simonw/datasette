9683aeb2394a4b7e44499b8a0240af3baafda832
Simon Willison <swillison@gmail.com>
2019-05-03 22:05:42 -0400

    Do not attempt 'import black' for Python 3.5

--- tests/test_black.py
+++ tests/test_black.py
@@ -1,4 +1,3 @@
-import black
 from click.testing import CliRunner
 from pathlib import Path
 import pytest
@@ -11,6 +10,9 @@ code_root = Path(__file__).parent.parent
     sys.version_info[:2] < (3, 6), reason="Black requires Python 3.6 or later"
 )
 def test_black():
+    # Do not import at top of module because Python 3.5 will not have it installed
+    import black
+
     runner = CliRunner()
     result = runner.invoke(
         black.main, [str(code_root / "tests"), str(code_root / "datasette"), "--check"]

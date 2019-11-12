18cc795b5c6ec9223a5cbc4f30b19710c17f7eaa
Simon Willison <swillison@gmail.com>
2019-11-11 20:56:09 -0800

    Update docs to reflect we no longer support 3.5
    Also removed code that skipped black test for 3.5.

--- tests/test_black.py
+++ tests/test_black.py
@@ -1,3 +1,4 @@
+import black
 from click.testing import CliRunner
 from pathlib import Path
 import pytest
@@ -6,13 +7,7 @@ import sys
 code_root = Path(__file__).parent.parent
 
 
-@pytest.mark.skipif(
-    sys.version_info[:2] < (3, 6), reason="Black requires Python 3.6 or later"
-)
 def test_black():
-    # Do not import at top of module because Python 3.5 will not have it installed
-    import black
-
     runner = CliRunner()
     result = runner.invoke(
         black.main, [str(code_root / "tests"), str(code_root / "datasette"), "--check"]

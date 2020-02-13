cf5f4386ef6fea9179bbaaec2e187917934759d7
Simon Willison <swillison@gmail.com>
2020-02-13 15:02:10 -0800

    Run black against everything, not just tests and datasette dirs

--- tests/test_black.py
+++ tests/test_black.py
@@ -9,7 +9,5 @@ code_root = Path(__file__).parent.parent
 
 def test_black():
     runner = CliRunner()
-    result = runner.invoke(
-        black.main, [str(code_root / "tests"), str(code_root / "datasette"), "--check"]
-    )
+    result = runner.invoke(black.main, [str(code_root), "--check"])
     assert result.exit_code == 0, result.output

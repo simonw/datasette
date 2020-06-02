29b8e80f3c7c9f49fa30141e3fa2d2619d2683c3
Simon Willison <swillison@gmail.com>
2020-06-02 14:06:53 -0700

    New request.cookies property

--- datasette/actor_auth_cookie.py
+++ datasette/actor_auth_cookie.py
@@ -5,14 +5,9 @@ from http.cookies import SimpleCookie
 
 @hookimpl
 def actor_from_request(datasette, request):
-    cookies = SimpleCookie()
-    cookies.load(
-        dict(request.scope.get("headers") or []).get(b"cookie", b"").decode("utf-8")
-    )
-    if "ds_actor" not in cookies:
+    if "ds_actor" not in request.cookies:
         return None
-    ds_actor = cookies["ds_actor"].value
     try:
-        return datasette.unsign(ds_actor, "actor")
+        return datasette.unsign(request.cookies["ds_actor"], "actor")
     except BadSignature:
         return None

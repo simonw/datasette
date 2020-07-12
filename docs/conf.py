ee0ef016523a765b6ef6eaa43cad9ad568f78ae4
Simon Willison <swillison@gmail.com>
2020-07-12 12:53:29 -0700

    Added new logo to the documentation

--- docs/conf.py
+++ docs/conf.py
@@ -91,13 +91,19 @@ html_theme = "sphinx_rtd_theme"
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#
-# html_theme_options = {}
+html_theme_options = {
+    "logo_only": True,
+    "style_nav_header_background": "white",
+    "prev_next_buttons_location": "both",
+}
+
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+html_logo = "datasette-logo.svg"
 
 html_css_files = [
     "css/custom.css",

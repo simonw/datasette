20b1de86a1e95e3f98215f17a247cba5311007ae
Simon Willison <swillison@gmail.com>
2020-09-11 15:04:23 -0700

    Fix for test I broke in #947

--- tests/conftest.py
+++ tests/conftest.py
@@ -30,6 +30,7 @@ def pytest_collection_modifyitems(items):
     move_to_front(items, "test_black")
     move_to_front(items, "test_inspect_cli")
     move_to_front(items, "test_serve_with_get")
+    move_to_front(items, "test_serve_with_get_exit_code_for_error")
     move_to_front(items, "test_inspect_cli_writes_to_file")
     move_to_front(items, "test_spatialite_error_if_attempt_to_open_spatialite")
     move_to_front(items, "test_package")

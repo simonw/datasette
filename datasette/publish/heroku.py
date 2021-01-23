25c2933667680db045851b2cedcf4666d737d352
Simon Willison <swillison@gmail.com>
2021-01-22 16:46:16 -0800

    publish heroku now uses python-3.8.7

--- datasette/publish/heroku.py
+++ datasette/publish/heroku.py
@@ -173,7 +173,7 @@ def temporary_heroku_directory(
         if metadata_content:
             open("metadata.json", "w").write(json.dumps(metadata_content, indent=2))
 
-        open("runtime.txt", "w").write("python-3.8.6")
+        open("runtime.txt", "w").write("python-3.8.7")
 
         if branch:
             install = [

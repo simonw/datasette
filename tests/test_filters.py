71af58d9392d42f45506cbc2370c1e9994126f63
Simon Willison <swillison@gmail.com>
2021-12-17 09:23:27 -0800

    Additional tests for through_filters, where_filters, search_filters

--- tests/test_filters.py
+++ tests/test_filters.py
@@ -1,4 +1,6 @@
-from datasette.filters import Filters
+from datasette.filters import Filters, through_filters, where_filters, search_filters
+from datasette.utils.asgi import Request
+from .fixtures import app_client
 import pytest
 
 
@@ -74,3 +76,86 @@ def test_build_where(args, expected_where, expected_params):
     sql_bits, actual_params = f.build_where_clauses("table")
     assert expected_where == sql_bits
     assert {f"p{i}": param for i, param in enumerate(expected_params)} == actual_params
+
+
+@pytest.mark.asyncio
+async def test_through_filters_from_request(app_client):
+    request = Request.fake(
+        '/?_through={"table":"roadside_attraction_characteristics","column":"characteristic_id","value":"1"}'
+    )
+    filter_args = await (
+        through_filters(
+            request=request,
+            datasette=app_client.ds,
+            table="roadside_attractions",
+            database="fixtures",
+        )
+    )()
+    assert filter_args.where_clauses == [
+        "pk in (select attraction_id from roadside_attraction_characteristics where characteristic_id = :p0)"
+    ]
+    assert filter_args.params == {"p0": "1"}
+    assert filter_args.human_descriptions == [
+        'roadside_attraction_characteristics.characteristic_id = "1"'
+    ]
+    assert filter_args.extra_context == {}
+
+
+@pytest.mark.asyncio
+async def test_through_filters_from_request(app_client):
+    request = Request.fake(
+        '/?_through={"table":"roadside_attraction_characteristics","column":"characteristic_id","value":"1"}'
+    )
+    filter_args = await (
+        through_filters(
+            request=request,
+            datasette=app_client.ds,
+            table="roadside_attractions",
+            database="fixtures",
+        )
+    )()
+    assert filter_args.where_clauses == [
+        "pk in (select attraction_id from roadside_attraction_characteristics where characteristic_id = :p0)"
+    ]
+    assert filter_args.params == {"p0": "1"}
+    assert filter_args.human_descriptions == [
+        'roadside_attraction_characteristics.characteristic_id = "1"'
+    ]
+    assert filter_args.extra_context == {}
+
+
+@pytest.mark.asyncio
+async def test_where_filters_from_request(app_client):
+    request = Request.fake("/?_where=pk+>+3")
+    filter_args = await (
+        where_filters(
+            request=request,
+            datasette=app_client.ds,
+            database="fixtures",
+        )
+    )()
+    assert filter_args.where_clauses == ["pk > 3"]
+    assert filter_args.params == {}
+    assert filter_args.human_descriptions == []
+    assert filter_args.extra_context == {
+        "extra_wheres_for_ui": [{"text": "pk > 3", "remove_url": "/"}]
+    }
+
+
+@pytest.mark.asyncio
+async def test_search_filters_from_request(app_client):
+    request = Request.fake("/?_search=bobcat")
+    filter_args = await (
+        search_filters(
+            request=request,
+            datasette=app_client.ds,
+            database="fixtures",
+            table="searchable",
+        )
+    )()
+    assert filter_args.where_clauses == [
+        "rowid in (select rowid from searchable_fts where searchable_fts match escape_fts(:search))"
+    ]
+    assert filter_args.params == {"search": "bobcat"}
+    assert filter_args.human_descriptions == ['search matches "bobcat"']
+    assert filter_args.extra_context == {"supports_search": True, "search": "bobcat"}

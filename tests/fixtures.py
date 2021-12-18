simonw/datasette

commit c35b84a2aabe2f14aeacf6cda4110ae1e94d6059

Remove undocumented sqlite_functions mechanism, closes #1567

tests/fixtures.py
@@ -153,7 +153,6 @@ def make_app_client(
             template_dir=template_dir,
             crossdb=crossdb,
         )
-        ds.sqlite_functions.append(("sleep", 1, lambda n: time.sleep(float(n))))
         yield TestClient(ds)
 
 

datasette/app.py
@@ -259,7 +259,6 @@ class Datasette:
             with metadata_files[0].open() as fp:
                 metadata = parse_metadata(fp.read())
         self._metadata_local = metadata or {}
-        self.sqlite_functions = []
         self.sqlite_extensions = []
         for extension in sqlite_extensions or []:
             # Resolve spatialite, if requested
@@ -548,8 +547,6 @@ class Datasette:
     def _prepare_connection(self, conn, database):
         conn.row_factory = sqlite3.Row
         conn.text_factory = lambda x: str(x, "utf-8", "replace")
-        for name, num_args, func in self.sqlite_functions:
-            conn.create_function(name, num_args, func)
         if self.sqlite_extensions:
             conn.enable_load_extension(True)
             for extension in self.sqlite_extensions:

tests/plugins/sleep_sql_function.py
@@ -1,0 +1,7 @@
+from datasette import hookimpl
+import time
+
+
+@hookimpl
+def prepare_connection(conn):
+    conn.create_function("sleep", 1, lambda n: time.sleep(float(n)))

commit 3a0cae4d7f77b5c2a103ea74ca7fa7a0d9ff2e66

Fix bug introduced by refactor in c35b84a2aabe2f14aeacf6cda4110ae1e94d6059

tests/fixtures.py
@@ -84,6 +84,13 @@ EXPECTED_PLUGINS = [
         "hooks": ["register_output_renderer"],
     },
     {
+        "name": "sleep_sql_function.py",
+        "static": False,
+        "templates": False,
+        "version": None,
+        "hooks": ["prepare_connection"],
+    },
+    {
         "name": "view_name.py",
         "static": False,
         "templates": False,
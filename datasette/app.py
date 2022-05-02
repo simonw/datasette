fb26057dd1e4badbaa9c9887c41f4a7a8d777984
Simon Willison <swillison@gmail.com>
2022-05-02 12:20:14 -0700

    Optional uvicorn import for Pyodide, refs #1733

--- datasette/app.py
+++ datasette/app.py
@@ -804,6 +804,15 @@ class Datasette:
         datasette_version = {"version": __version__}
         if self.version_note:
             datasette_version["note"] = self.version_note
+
+        try:
+            # Optional import to avoid breaking Pyodide
+            # https://github.com/simonw/datasette/issues/1733#issuecomment-1115268245
+            import uvicorn
+
+            uvicorn_version = uvicorn.__version__
+        except ImportError:
+            uvicorn_version = None
         info = {
             "python": {
                 "version": ".".join(map(str, sys.version_info[:3])),
@@ -811,6 +820,7 @@ class Datasette:
             },
             "datasette": datasette_version,
             "asgi": "3.0",
+            "uvicorn": uvicorn_version,
             "sqlite": {
                 "version": sqlite_version,
                 "fts_versions": fts_versions,

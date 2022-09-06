b2b901e8c4b939e50ee1117ffcd2881ed8a8e3bf
Simon Willison <swillison@gmail.com>
2022-09-05 17:05:23 -0700

    Skip SpatiaLite test if no conn.enable_load_extension()
    Ran into this problem while working on #1802

--- tests/test_spatialite.py
+++ tests/test_spatialite.py
@@ -1,5 +1,6 @@
 from datasette.app import Datasette
 from datasette.utils import find_spatialite, SpatialiteNotFound, SPATIALITE_FUNCTIONS
+from .utils import has_load_extension
 import pytest
 
 
@@ -13,6 +14,7 @@ def has_spatialite():
 
 @pytest.mark.asyncio
 @pytest.mark.skipif(not has_spatialite(), reason="Requires SpatiaLite")
+@pytest.mark.skipif(not has_load_extension(), reason="Requires enable_load_extension")
 async def test_spatialite_version_info():
     ds = Datasette(sqlite_extensions=["spatialite"])
     response = await ds.client.get("/-/versions.json")

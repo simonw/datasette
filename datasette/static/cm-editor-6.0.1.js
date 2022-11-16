fed8526fa55a4de0aca00486f1447b2498d0eb08
Brian Grinstead <briangrinstead@gmail.com>
2022-11-16 08:20:33 -0800

    update dialect

--- datasette/static/cm-editor-6.0.1.js
+++ datasette/static/cm-editor-6.0.1.js
@@ -4,13 +4,13 @@ import { sql, SQLDialect } from "@codemirror/lang-sql";
 
 // A variation of SQLite from lang-sql https://github.com/codemirror/lang-sql/blob/ebf115fffdbe07f91465ccbd82868c587f8182bc/src/sql.ts#L231
 const SQLite = SQLDialect.define({
-  // https://www.sqlite.org/lang_keywords.html
+  // Based on https://www.sqlite.org/lang_keywords.html based on likely keywords to be used in select queries
+  // https://github.com/simonw/datasette/pull/1893#issuecomment-1316401895:
   keywords:
-    "abort action add after all alter always analyze and as asc attach autoincrement before begin between by cascade case cast check collate column commit conflict constraint create cross current current_date current_time current_timestamp database default deferrable deferred delete desc detach distinct do drop each else end escape except exclude exclusive exists explain fail filter first following for foreign from full generated glob group groups having if ignore immediate in index indexed initially inner insert instead intersect into is isnull join key last left like limit match materialized natural no not nothing notnull null nulls of offset on or order others outer over partition plan pragma preceding primary query raise range recursive references regexp reindex release rename replace restrict returning right rollback row rows savepoint select set table temp temporary then ties to transaction trigger unbounded union unique update using vacuum values view virtual when where window with without",
+    "and as asc between by case cast count current_date current_time current_timestamp desc distinct each else escape except exists explain filter first for from full generated group having if in index inner intersect into isnull join last left like limit not null or order outer over pragma primary query raise range regexp right rollback row select set table then to union unique using values view virtual when where",
   // https://www.sqlite.org/datatype3.html
   types: "null integer real text blob",
-  builtin:
-    "auth backup bail changes clone databases dbinfo dump echo eqp explain fullschema headers help import imposter indexes iotrace lint load log mode nullvalue once print prompt quit restore save scanstats separator shell show stats system tables testcase timeout timer trace vfsinfo vfslist vfsname width",
+  builtin: "",
   operatorChars: "*+-%<>!=&|/~",
   identifierQuotes: '`"',
   specialVar: "@:?$",

dca7634c274e71fae0d95ea4ff1dfd987b9af63c
Colin Dellow <cldellow@gmail.com>
2023-01-25 00:04:17 -0500

    use single quotes for string literals, fixes #2001

--- tests/test_api.py
+++ tests/test_api.py
@@ -880,7 +880,7 @@ test_json_columns_default_expected = [
 )
 async def test_json_columns(ds_client, extra_args, expected):
     sql = """
-        select 1 as intval, "s" as strval, 0.5 as floatval,
+        select 1 as intval, 's' as strval, 0.5 as floatval,
         '{"foo": "bar"}' as jsonval
     """
     path = "/fixtures.json?" + urllib.parse.urlencode({"sql": sql, "_shape": "array"})

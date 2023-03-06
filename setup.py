829703218fcba00b8b7929615fe53e9c65df1cb9
Dustin Rodrigues <dust.rod@gmail.com>
2023-03-06 16:01:19 -0500

    Add Python 3.11 classifier (#2028)
    Thanks, @dtrodrigues

--- setup.py
+++ setup.py
@@ -96,6 +96,7 @@ setup(
         "Intended Audience :: End Users/Desktop",
         "Topic :: Database",
         "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.8",

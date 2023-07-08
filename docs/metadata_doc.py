3b336d8071fb5707bd006de1d614f701d20246a3
Simon Willison <swillison@gmail.com>
2023-07-08 09:37:47 -0700

    Utility function for cog for generating YAML/JSON tabs, refs #1153

---
+++ docs/metadata_doc.py
@@ -0,0 +1,13 @@
+import json
+import textwrap
+import yaml
+
+
+def metadata_example(cog, example):
+    cog.out("\n.. tab:: YAML\n\n")
+    cog.out("    .. code-block:: yaml\n\n")
+    cog.out(textwrap.indent(yaml.dump(example), "        "))
+    cog.out("\n\n.. tab:: JSON\n\n")
+    cog.out("    .. code-block:: json\n\n")
+    cog.out(textwrap.indent(json.dumps(example, indent=2), "        "))
+    cog.out("\n")

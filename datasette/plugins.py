6bfe104d47b888c70bfb7781f8f48ff11452b2b5
Simon Willison <swillison@gmail.com>
2023-08-30 15:12:24 -0700

    DATASETTE_LOAD_PLUGINS environment variable for loading specific plugins
    Closes #2164
    
    * Load only specified plugins for DATASETTE_LOAD_PLUGINS=datasette-one,datasette-two
    * Load no plugins if DATASETTE_LOAD_PLUGINS=''
    * Automated tests in a Bash script for DATASETTE_LOAD_PLUGINS

--- datasette/plugins.py
+++ datasette/plugins.py
@@ -1,4 +1,5 @@
 import importlib
+import os
 import pluggy
 import pkg_resources
 import sys
@@ -22,9 +23,29 @@ DEFAULT_PLUGINS = (
 pm = pluggy.PluginManager("datasette")
 pm.add_hookspecs(hookspecs)
 
-if not hasattr(sys, "_called_from_test"):
+DATASETTE_LOAD_PLUGINS = os.environ.get("DATASETTE_LOAD_PLUGINS", None)
+
+if not hasattr(sys, "_called_from_test") and DATASETTE_LOAD_PLUGINS is None:
     # Only load plugins if not running tests
     pm.load_setuptools_entrypoints("datasette")
+
+# Load any plugins specified in DATASETTE_LOAD_PLUGINS")
+if DATASETTE_LOAD_PLUGINS is not None:
+    for package_name in [
+        name for name in DATASETTE_LOAD_PLUGINS.split(",") if name.strip()
+    ]:
+        try:
+            distribution = pkg_resources.get_distribution(package_name)
+            entry_map = distribution.get_entry_map()
+            if "datasette" in entry_map:
+                for plugin_name, entry_point in entry_map["datasette"].items():
+                    mod = entry_point.load()
+                    pm.register(mod, name=entry_point.name)
+                    # Ensure name can be found in plugin_to_distinfo later:
+                    pm._plugin_distinfo.append((mod, distribution))
+        except pkg_resources.DistributionNotFound:
+            sys.stderr.write("Plugin {} could not be found\n".format(package_name))
+
 
 # Load default plugins
 for plugin in DEFAULT_PLUGINS:

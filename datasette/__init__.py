bc52aaed24a96071550034e95d225c1f9965b619
Ian Chartier <ichartier@student.bridgew.edu>
2024-04-29 20:58:32 -0400

    Issue 1903 Fix 3
    Fixed issue within the init file, unknown to me previously.

--- datasette/__init__.py
+++ datasette/__init__.py
@@ -1,7 +1,7 @@
 from datasette.permissions import Permission  # noqa
 from datasette.version import __version_info__, __version__  # noqa
 from datasette.events import Event  # noqa
-from datasette.utils.asgi import Forbidden, NotFound, Request, Response  # noqa
+from datasette.views.error_module import Forbidden,NotFound,Response, Request
 from datasette.utils import actor_matches_allow  # noqa
 from datasette.views import Context  # noqa
 from .hookspecs import hookimpl  # noqa

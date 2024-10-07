bbc49c0f80925d90a408e0dbcecbe3ab883bab29
Simon Willison <swillison@gmail.com>
2024-10-07 10:08:14 -0700

    Drop Python 3.8, refs #2434

--- setup.py
+++ setup.py
@@ -99,9 +99,10 @@ setup(
         "Intended Audience :: End Users/Desktop",
         "Topic :: Database",
         "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.7",
     ],
 )

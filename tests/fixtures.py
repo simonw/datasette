232a404743ad007285b02838c222845ee4d39cbd
Simon Willison <swillison@gmail.com>
2025-12-12 22:18:35 -0800

    Switch searchable_fts test table to FTS5, closes #2613

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -536,9 +536,8 @@ INSERT INTO searchable_tags (searchable_id, tag) VALUES
 ;
 
 CREATE VIRTUAL TABLE "searchable_fts"
-    USING FTS4 (text1, text2, [name with . and spaces], content="searchable");
-INSERT INTO "searchable_fts" (rowid, text1, text2, [name with . and spaces])
-    SELECT rowid, text1, text2, [name with . and spaces] FROM searchable;
+    USING FTS5 (text1, text2, [name with . and spaces], content="searchable", content_rowid="pk");
+INSERT INTO "searchable_fts" (searchable_fts) VALUES ('rebuild');
 
 CREATE TABLE [select] (
   [group] text,

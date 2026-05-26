0cadd071871ef0b33e4ce3a23e316a104b3137c3
Simon Willison <swillison@gmail.com>
2026-05-26 14:53:31 -0700

    No need to document QueryCreateAnalyzeView

--- tests/test_docs.py
+++ tests/test_docs.py
@@ -66,7 +66,14 @@ def documented_views():
             if first_word.endswith("View"):
                 view_labels.add(first_word)
     # We deliberately don't document these:
-    view_labels.update(("PatternPortfolioView", "AuthTokenView", "ApiExplorerView"))
+    view_labels.update(
+        (
+            "PatternPortfolioView",
+            "AuthTokenView",
+            "ApiExplorerView",
+            "QueryCreateAnalyzeView",
+        )
+    )
     return view_labels
 
 

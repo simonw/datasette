de37f1451f70c46092077c8ec2919a4a3ad48eb6
Simon Willison <swillison@gmail.com>
2026-09-17 13:28:24 -0700

    Refactor the column type dialog to use the shared modal, refs #2790

--- tests/test_playwright.py
+++ tests/test_playwright.py
@@ -1763,7 +1763,7 @@ def test_modal_lifecycle(page, datasette_server, shadow):
 
 
 @pytest.mark.playwright
-@pytest.mark.parametrize("name", ["jump", "columns", "mobile"])
+@pytest.mark.parametrize("name", ["jump", "columns", "type", "mobile"])
 def test_modal_consumers_dismiss_and_restore_focus(page, datasette_server, name):
     from playwright.sync_api import expect
 
